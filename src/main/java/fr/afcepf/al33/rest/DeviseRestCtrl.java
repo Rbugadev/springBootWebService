3a92bd2fec302fab57d6a09c2d2e51789101b960
Roque rbugadev@gmail.com <rbugadev@gmail.com>
2019-04-17 09:24:25 +0200

    DeviseRestCtrl.java

--- src/main/java/fr/afcepf/al33/rest/DeviseRestCtrl.java
+++ src/main/java/fr/afcepf/al33/rest/DeviseRestCtrl.java
@@ -1,38 +1,49 @@
 package fr.afcepf.al33.rest;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import fr.afcepf.al33.conv.Convertisseur;
 import fr.afcepf.al33.dao.DeviseDao;
 import fr.afcepf.al33.entity.Devise;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController // Component de type @RestController
-@RequestMapping(value = "/rest/devises", headers = "Accept=application/json")
+@RestController //@Component de type @RestController
+@RequestMapping(value="/rest/devises" , headers="Accept=application/json")
 public class DeviseRestCtrl {
 
-    @Autowired // injection du business service
-    private Convertisseur convertisseur;
+	@Autowired //injection du "business service" 
+	private Convertisseur convertisseur;
+	
+	@Autowired //injection le dao (temporairement) 
+	private DeviseDao deviseDao;
+	
+	//URL= http://localhost:8080/springBootWebService/rest/devises/EUR
+	@RequestMapping(value="/{codeDevise}" , method=RequestMethod.GET)
+	public Devise getDeviseByCode(@PathVariable("codeDevise") String codeDevise) {
+		return deviseDao.findById(codeDevise).get();
+	}
+	
+	//URL= http://localhost:8080/springBootWebService/rest/devises
+	//URL= http://localhost:8080/springBootWebService/rest/devises?tauxChangeMini=1
+	@RequestMapping(value="" , method=RequestMethod.GET)
+	public List<Devise> getDevises(@RequestParam(value="tauxChangeMini",
+	                               required=false) Double tauxChangeMini) {
+		
+		List<Devise> toutesDevises = (List<Devise>) deviseDao.findAll();
+		
+		if(tauxChangeMini==null)
+			return toutesDevises;
+		else
+			return toutesDevises.stream()
+					  .filter((d)-> d.getTauxChange()>tauxChangeMini)
+					  .collect(Collectors.toList());
+	}
 
-    @Autowired
-    private DeviseDao deviseDao;
-
-    // URL = http://localhost:8080/springBootWebService/rest/devises/EUR
-    @RequestMapping(value = "/{codeDevise}", method = RequestMethod.GET)
-    public Devise getDeviseByCode(@PathVariable("codeDevise") String codeDevise){
-        return  deviseDao.findById(codeDevise).get();
-    }
-
-    // URL = http://localhost:8080/springBootWebService/rest/devises
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public List<Devise> getDevises(){
-        return (List<Devise>) deviseDao.findAll();
-    }
-
-    // URL = http://localhost:8080/springBootWebService/rest/devises/{tauxChange}
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public List<Devise> getDevisesTauxChangeMin(@RequestParam(value = "tauxChangeMin", required = false) double tauxChangeMin){
-        return (List<Devise>) deviseDao.findAll();
-    }
 }
